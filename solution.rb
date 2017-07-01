0240ca518974591030d3c0ff65f022771558570b
Maria De Vuono <medevuono@gmail.com>
2017-07-01 11:37:23 -0500

    solucion al reto cual es mi navegador. make it real

--- solution.rb
+++ solution.rb
@@ -1,7 +1,8 @@
  require "sinatra"
 
-get '/' do
-  
+get '/' do  
+  #@agent = request.user_agent
+  #erb :index
   request.env['HTTP_PERMISO'] == "soy-un-token-secreto" ? response = "Si lo logramos!" : response = "Sin Permiso"
   response
 end
