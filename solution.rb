f6c5650c4a0b458370e4f72c59a1265125d39605
Maria De Vuono <medevuono@gmail.com>
2017-07-01 11:29:31 -0500

    solucion al reto Sin Permiso de sinatra. make it real

--- solution.rb
+++ solution.rb
@@ -1,5 +1,7 @@
-require 'sinatra'
+ require "sinatra"
 
 get '/' do
-    @agent = request.user_agent
+  
+  request.env['HTTP_PERMISO'] == "soy-un-token-secreto" ? response = "Si lo logramos!" : response = "Sin Permiso"
+  response
 end
